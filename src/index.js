509296cc20a52b6945afd34087d2dc073293cec4
Luis Escobar <dukz@MacBook-Pro-de-Guiseppe.local>
2024-06-30 19:33:41 -0500

    Se agrego el usuario modificado 2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const morgan = require('morgan');
 const connectDatabase = require('./database');
 const v1PublicacionRouter = require('./routers/publicacion.router');
-const v1AuthRouter = require('./routers/auth.router');
 
 const app = express();
 
@@ -18,7 +17,6 @@ app.use(cors());
 app.use(express.json());
 
 // rutas de la aplicacion
-app.use('/api/v1/auth', v1AuthRouter);
 app.use('/api/v1/publicacion', v1PublicacionRouter);
 
 // configuracion del puerto
